7decabb09c5474ee699f699209550e3a2810e598
jerry-weng <jjweng2007@gmail.com>
2016-11-23 15:35:20 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# GY-271 Driver
+
+3-axis digital compass sensor driver.
+
+## Device Model
+
+- [gy-271](https://rap.ruff.io/devices/gy-271)
+
+## Install
+
+```sh
+> rap device add --model gy-271 --id <device-id>
+```
+
+## Usage
+
+```js
+$('#<device-id>').getHeading(function (error, value) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log(value);
+});
+```
+
+## API References
+
+### Methods
+
+#### `getHeading(callback)`
+
+Get the heading of the sensor with respect to the X axis.
+
+- **callback:** The callback that takes the first argument as the possible error and the second argument as the heading value in degree.
+The range of the heading is [0, 360).
+
+#### `getMagneticField(callback)`
+
+Get the magnetic field measurement by the sensor.
+
+- **callback:** The callback that takes the first argument as the possible error and the second argument as the magnetic vlaue.
+The magnetic value is an array composed with 3 elements that represent x, y and z axis data respectively.
+
+#### `getIdentification(callback)`
+
+Get the identification of the chip `HMC5883L`, which is fixed with `H43`.
+
+## Supported OS
+
+Test passed on Ruff v1.6.1
+
+## Note
+
+Make sure the sensor is parallel to the ground, thus the heading from `getHeading()` is a significant value.
---
+++ src/index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var driver = require('ruff-driver');
+
+var Resolution = [
+    0.73,
+    0.92,
+    1.22,
+    1.52,
+    2.27,
+    2.56,
+    3.03,
+    4.35
+];
+
+var Addr = {
+    cra: 0,
+    crb: 1,
+    mr: 2,
+    data: 3,
+    sr: 9,
+    ir: 10
+}
+
+module.exports = driver({
+    attach: function (inputs, context, next) {
+        var that = this;
+        this._i2c = inputs['i2c'];
+
+        this._resolution = Resolution[1];
+        this._setOperationMode(next);
+
+    },
+    exports: {
+        _setOperationMode: function (callback) {
+            this._i2c.writeByte(Addr.cra, 0x70);
+            this._i2c.writeByte(Addr.mr, 0x00, callback);
+        },
+        getHeading: function (callback) {
+            this.getMagneticField(function (error, value) {
+                if (error) {
+                    callback(error);
+                    return;
+                }
+                var radian = Math.atan2(value[1], value[0]);
+                if (radian < 0) {
+                    radian += 2 * Math.PI;
+                }
+                callback(undefined, (radian * 180 / Math.PI).toFixed(2) - 0);
+            });
+        },
+        getMagneticField: function (callback) {
+            var that = this;
+            this._i2c.readBytes(Addr.data, 6, function (error, value) {
+                callback(error, [
+                    wordToNum(value.slice(0, 2)),
+                    wordToNum(value.slice(4)),
+                    wordToNum(value.slice(2, 4))
+                ].map(function (value) {
+                    return value * that._resolution;
+                }));
+            });
+        },
+        getIdentification: function (callback) {
+            this._i2c.readBytes(Addr.ir, 3, function (error, value) {
+                if (error) {
+                    callback(error);
+                    return;
+                }
+                callback(undefined, String.fromCharCode.apply(undefined, value));
+            });
+        }
+    }
+});
+
+function wordToNum(word) {
+    var num = word[0] << 8 | word[1];
+    if (word[0] >= 0x80) {
+        return num - Math.pow(2, 16);
+    }
+    return num;
+}
